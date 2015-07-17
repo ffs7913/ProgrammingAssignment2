d15a956b0ddc1c8821636ca5ec9d1f592579d5f3
Sidney <sidneyfletcher@gmail.com>
2015-07-17 14:45:33 -0400

    test

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ## Goofy program to cache a matrix inverse
 
 
@@ -33,20 +32,4 @@ cacheSolve <- function(x, ...) {
         }
         
         inv
-=======
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
->>>>>>> 7f657dd22ac20d22698c53b23f0057e1a12c09b7
 }
